12755dd23a5e54699861a3fc5fa81b07690ce6fd
Hồ Lữ <lu.ho@aegona.com>
2020-09-09 14:40:51 +0700

    init proj

--- Example/Demo4T/ViewController.swift
+++ Example/Demo4T/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        TestViewController.init()
+//        TestViewController.init()
+        TestFile.init()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ HomeController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeController.swift
+//  Demo4T
+//
+//  Created by AEGONA on 9/9/20.
+//
+
+import Foundation
