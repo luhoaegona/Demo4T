ac426b5dc27f6aab3d182d1ff2eeb97f62409ce1
Hồ Lữ <lu.ho@aegona.com>
2020-09-09 14:44:57 +0700

    init proj

---
+++ TestProjT/TestProjT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProjT
+//
+//  Created by AEGONA on 9/9/20.
+//  Copyright © 2020 AEGONA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
